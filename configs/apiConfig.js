00cf695a153b3f627122168ff9d864c2214bbf81
Nolstan Kumwenda <nolstankumwenda@gmail.com>
2026-01-30 19:20:01 +0200

    responsiveness of index.html

--- configs/apiConfig.js
+++ configs/apiConfig.js
@@ -4,4 +4,4 @@
  * For production, this would be an empty string '' if the frontend is served
  * from the same domain as the backend, or your production API domain.
  */
-export const API_BASE_URL = 'https://stayhub-1hqw.onrender.com';
+export const API_BASE_URL = 'https://fada-house.onrender.com'
